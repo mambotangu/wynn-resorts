338fed880a43e71ac05346738d90dac5ae5b1df9
mambotangu <92550548+mambotangu@users.noreply.github.com>
2022-05-02 03:11:53 -0400

    Update main.tf

--- organization/main.tf
+++ organization/main.tf
@@ -1,11 +1,18 @@
+resource "google_folder" "folders" {
+  for_each = toset(local.folders)
+
+  display_name = each.value
+  parent       = "organizations/${data.terraform_remote_state.bootstrap.outputs.organization_id}"
+}
+
 module "organization_policies" {
-  source = "../../modules/organization_policies"
-  org_domain_id        = var.org_domain_id
+  source = "../modules/organization_policies"
+
   organization_id = data.terraform_remote_state.bootstrap.outputs.organization_id
 }
 
 module "org_level_iam_group_bindings" {
-  source               = "../../modules/iam/org-iam"
+  source               = "../modules/iam/org-iam"
   domain               = var.domain
   org_id               = var.organization_id
   billing_admin_group  = var.billing_admin_group
